0a5debe31cde7de0d9b2630cb6e2145b08513678
Paul Beeler <pbeeler80@gmail.com>
2015-04-27 00:35:45 -0600

    sm.mk: code shrink
    Change-Id: Ie2b71913bba7bf8557a4fb9b4b22896eeb0c7ca3
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- core/sabermod/sm_clear_vars.mk
+++
@@ -1,8 +0,0 @@
-###########################################################
-## Clear out values of all variables used by rule templates.
-###########################################################
-
-$(shell unset LOCAL_O3_OPTIMIZATIONS_MODE)
-$(shell unset LOCAL_TMP_ARM_MODE)
-$(shell unset LOCAL_TMP_ARCH)
-
